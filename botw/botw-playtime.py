b915ba30aa8c61c86ede3f38ebd7d7caa7c6c3d2
Fabio Grassiotto <fabiogr@gmail.com>
2020-10-05 14:22:57 -0300

    Update botw-playtime.py

--- botw/botw-playtime.py
+++ botw/botw-playtime.py
@@ -1,17 +1,34 @@
 import os
 import glob
+import json
+import time
+import datetime
+import sav_to_json as sv
 
-save_dir = "C:/Games/The Legend of Zelda - Breath of the Wild/cemu/mlc01/usr/save/00050000/101c9500/user/80000001"
+#save_dir = "C:/Games/The Legend of Zelda - Breath of the Wild/cemu/mlc01/usr/save/00050000/101c9500/user/80000001"
+save_dir = "C:/tmp"
 
 # Using '*' pattern  
-print('Savegame:') 
 files = glob.glob(save_dir + '/**/game_data.sav', recursive=True) 
 
 # Check for empty list.
 if not files:
-    print("Not found")
+    print("Savegames not found.")
     exit()
 
 # Last modified time
 latest_file = max(files, key=os.path.getmtime)
-print(latest_file)
+print('Savegame: ' + latest_file) 
+
+# convert to json
+sv.convert_to_json(latest_file)
+
+# Read playtime from json
+with open('game_data.sav.json') as json_file:
+    data = json.load(json_file)
+
+playtime = data['PlayReport_PlayTime']
+
+print("Your current Botw playtime: " + str(datetime.timedelta(seconds=playtime)))
+time.sleep(3)
+print("Bye")
