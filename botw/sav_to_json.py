f5774100f0d26419a6a75eaccd9a0b5eed140001
Fabio Grassiotto <fabiogr@gmail.com>
2020-10-05 15:25:42 -0300

    Update sav_to_json.py

--- botw/sav_to_json.py
+++ botw/sav_to_json.py
@@ -11,7 +11,9 @@ def convert_to_json(file):
         if str(fileObj).split('.')[-1] == 'sav':
             dataOut = save.parseSaveFile(fileObj, skip_bools=False)
             if dataOut != None:
-                fileOpen = open(pathlib.Path(str(f'{fileObj}.json')), 'wt')
+                path = pathlib.Path(str(f'{fileObj}.json'))
+                file_name = path.name
+                fileOpen = open(file_name, 'wt')
                 fileOpen.write(json.dumps(dataOut, indent=2))
                 fileOpen.close()
             else:
